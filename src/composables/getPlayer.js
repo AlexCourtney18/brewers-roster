0a18e99e2d09dd0eb4974c8122f0fa0c9a469905
Alex <lex.courtney18@gmail.com>
2023-02-20 17:18:44 -0600

    add logic to getPlayer.js to update position, throws, and bats abreviation to full word

--- src/composables/getPlayer.js
+++ src/composables/getPlayer.js
@@ -3,6 +3,9 @@ import { ref } from 'vue'
 const getPlayer = (id) => {
     const player = ref([])
     const error = ref(null)
+    const position = ref(null)
+    const bats = ref(null)
+    const throws = ref(null)
 
     const load = async () => {
       try {
@@ -11,6 +14,22 @@ const getPlayer = (id) => {
           throw Error('no data available')
         }
         player.value = await data.json()
+        if (player.value.primaryPosition === "P") {
+            position.value = "Pitcher"
+        } else position.value = player.value.primaryPosition
+
+        if (player.value.batSide === "R") {
+            bats.value = "Right"
+        } else if (player.value.batSide === "L") {
+            bats.value = "Left"
+        }
+
+        if (player.value.throwSide === "L") {
+            throws.value = "Left"
+        } else if (player.value.throwSide === "R") {
+            throws.value = "Right"
+        }
+        
       }
       catch (err) {
         error.value = err.message
@@ -18,7 +37,9 @@ const getPlayer = (id) => {
       }
     }
 
-    return { player, error, load }
+    
+
+    return { player, error, position, bats, throws, load }
 }
 
 
--- src/views/PlayerDetails.vue
+++ src/views/PlayerDetails.vue
@@ -2,11 +2,11 @@
   <div v-if="error">{{ error }}</div>
   <div v-if="player" class="player">
     <h1>{{ player.firstName }} {{ player.lastName }}</h1>
-    <h2>Position: {{ player.primaryPosition }}</h2>
+    <h2>Position: {{ position }}</h2>
     <img :src="player.picture" alt="" @click="handleClick" />
     <ul>
-      <li>Bats {{ player.batSide }}</li>
-      <li>Throws {{ player.throwSide }}</li>
+      <li>Bats: {{ bats }}</li>
+      <li>Throws: {{ throws }}</li>
       <li>Wears Number # {{ player.number }}</li>
       <li>
         Born In {{ player.birthCity }}, {{ player.birthStateProvince }} -
@@ -19,14 +19,16 @@
 <script>
 import getPlayer from "../composables/getPlayer";
 
+
+
 export default {
-  props: ["id"],
+  props: ['id'],
   setup(props) {
-    const { player, error, load } = getPlayer(props.id);
+    const { player, error, position, bats, throws, load } = getPlayer(props.id);
 
     load();
 
-    return { player, error };
+    return { player, error, position, bats, throws };
   },
 };
 </script>
